013255967083cd74b48add10b8cbbf299cd25a31
Melody <melodysu83@gmail.com>
2016-09-08 19:49:57 -0700

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -18,9 +18,14 @@ void functions::publish_automove(tf::Vector3 pos_incr, int id)
 {
 	raven_2::raven_automove rauto;
 	rauto.hdr.stamp = rauto.hdr.stamp.now();
+//      rauto.del_pos[0] = (int)pos_incr.x();
+//	rauto.del_pos[1] = (int)pos_incr.y();
+//	rauto.del_pos[2] = (int)pos_incr.z();
 
+
+	tf::Vector3 zero(0,0,0);
 	TF_INCR[0].setOrigin(pos_incr);
-	TF_INCR[1].setOrigin(pos_incr);
+	TF_INCR[1].setOrigin(zero);
 		
 	tf::Quaternion Delta_Ori(0,0,0,1);
 	TF_INCR[0].setRotation(Delta_Ori); 
@@ -47,9 +52,10 @@ void functions::Callback(raven_2::raven_state msg)
 
 		if(gtime==1) 
 		{
-			increment.setValue(0,0,-40000);
+			increment.setValue(0,0,-5000);
 			last_pos = current_pos;
-			des_pos = last_pos+increment;
+			des_pos = current_pos+increment;
+                        //ROS_INFO("distance start%f",(des_pos-current_pos).length());
 		}
 		else
 		{
@@ -59,8 +65,11 @@ void functions::Callback(raven_2::raven_state msg)
 			{
 			    tf::Vector3 inc = distance < maxin? pos_inc:pos_inc*maxin/distance;
 			    publish_automove(inc,0);
-				ROS_INFO("pub"); 
+                           // ROS_INFO("dest %f,%f,%f",des_pos.x(),des_pos.y(),des_pos.z());
+			   //ROS_INFO("current_pos %f,%f,%f",current_pos.x(),current_pos.y(),current_pos.z());
 			}
+
+                        //if(gtime%1000==0) ROS_INFO("distance %f",distance);
 		}
 	}
 }
